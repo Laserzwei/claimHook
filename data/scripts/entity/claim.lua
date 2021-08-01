b68783cb82c272c9d08df141a9de7c2d05117391
Laserzwei <lasernr2mail.com>
2021-08-01 15:44:18 +0200

    2.0 update
    beforeEndingTheScript(...) may return falsem to prevent the script from successfully changing ownership and terminating itself

--- data/scripts/entity/claim.lua
+++ data/scripts/entity/claim.lua
@@ -1,3 +1,5 @@
+local localGameVersion = GameVersion()
+
 -- Overwrite vanilla claim(), because it terminates itself.
 function claim()
     local ok, msg = interactionPossible(callingPlayer)
@@ -20,18 +22,30 @@ function claim()
     if entity.factionIndex ~= 0 then
         return false
     end
+    -- hook for other scripts
+    if beforeEndingTheScript(ok, msg, entity) == false then
+        return false
+    end
     entity.factionIndex = faction.index
     entity:addScriptOnce("minefounder.lua")
     entity:addScriptOnce("sellobject.lua")
     entity:setValue("valuable_object", nil)
     entity:setValue("map_marker", "Claimed Asteroid"%_T)
-    -- hook for other scripts
-    beforeEndingTheScript(ok, msg, entity)
+
+    if localGameVersion.major >= 2 then
+        local sector = Sector()
+        local x, y = sector:getCoordinates()
+        player:sendCallback("onAsteroidClaimed", makeCallbackSenderInfo(entity))
+    end
+
+
     terminate()
 end
 
 -- Allow other scripts to hook in.
 -- Has all local variables as arguments ('callingPlayer' is an implicit "global").
+-- Return false to prevent the Script from sending the Asteroid Claimed Notification and prevent it from self-termination
 function beforeEndingTheScript(ok, msg, entity)
     -- Dummy function to be overwritten by other mods.
+    return true
 end
